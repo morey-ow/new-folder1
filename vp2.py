fdaf3fd1cbdf9f0098ce21d5b4121192b03bbd22
yogesh <morey@oldwestbury.edu>
2022-02-17 17:33:59 -0500

    feb 17

--- vp2.py
+++ vp2.py
@@ -2,15 +2,15 @@ import vpython as vp
 import numpy as np
 
 xpos=0
-ypos=1
+ypos=3
 zpos=0
 ball=vp.sphere(make_trail=True, pos=vp.vector(xpos,ypos,zpos), color=vp.color.red, radius=0.5)
-ground=vp.box(length=200, height=1, width=100, color=vp.color.green, opacity=0.5)
+ground=vp.box(length=1200, height=2, width=100, color=vp.color.green, opacity=0.5)
 vp.arrow(axis=vp.vector(1,0,0), label='X')
 
 dt=0.01
-vx=5
-vy=10
+vx=7
+vy=24
 ay=-9.8
 t=0
 while ypos>0:
@@ -19,7 +19,7 @@ while ypos>0:
 	xpos=xpos+vx*t
 	ypos=ypos + vy*t+ ay*(t**2)
 	ball.pos=vp.vector(xpos, ypos, zpos)
-	print(f'{xpos, ypos, zpos}')
+	print(f'{xpos, ypos, zpos, t}')
 
 while True:
 	pass
